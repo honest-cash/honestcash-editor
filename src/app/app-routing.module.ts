5eb399029256275d4641f2927b5647cbd2781af2
Sercan AKMAN <sercanakman@gmail.com>
2019-02-11 16:37:14 +0300

    tbc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotAuthorizedComponent } from '@app/shared/components/pages/not-authorized/not-authorized.component';
 
 const routes: Routes = [
   // Fallback when no prior route is matched
