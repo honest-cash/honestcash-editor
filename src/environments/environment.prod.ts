3ed1bd6eed47d4f1f7bd34911158c421e2a20fdc
Sercan AKMAN <sercanakman@gmail.com>
2019-03-31 00:10:39 +0300

    lint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,8 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://honest.cash/api',
+  // serverUrl: 'https://honest.cash/api',
+  serverUrl: 'http://localhost:8080/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
