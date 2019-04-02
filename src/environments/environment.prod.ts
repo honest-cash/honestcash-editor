0d3ab56e0897d5676af1eef87db889fd7af457ae
Sercan AKMAN <sercanakman@gmail.com>
2019-04-02 23:52:40 +0300

    fixed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  // serverUrl: 'https://honest.cash/api',
-  serverUrl: 'http://localhost:8080/api',
+  serverUrl: 'https://honest.cash/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
